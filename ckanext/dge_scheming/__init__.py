19c5278fb750843d93481ee13fc40323dc3ab558
datos.gob.es <contacto@datos.gob.es>
2026-01-03 02:27:25 +0100

    chore(ckanext-dge): código legacy 06/2022

--- ckanext/dge_scheming/__init__.py
+++ ckanext/dge_scheming/__init__.py
@@ -14,11 +14,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-# this is a namespace package
-try:
-    import pkg_resources
-    pkg_resources.declare_namespace(__name__)
-except ImportError:
-    import pkgutil
-    __path__ = pkgutil.extend_path(__path__, __name__)
